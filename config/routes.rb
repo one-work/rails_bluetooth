aa91d2711390cedb4819ff64123af843e8e48864
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-11 20:01:19 +0800

    simple troute

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
-  scope RailsCom.default_routes_scope do
 
-    namespace 'bluetooth', defaults: { business: 'bluetooth' } do
-
-      resources :devices do
-        collection do
-          get :test
-          post :err
-        end
-      end
-      namespace :admin, defaults: { namespace: 'admin' } do
-        root 'home#index'
-        resources :devices
+  namespace 'bluetooth', defaults: { business: 'bluetooth' } do
+    resources :devices do
+      collection do
+        get :test
+        post :err
       end
     end
-
+    namespace :admin, defaults: { namespace: 'admin' } do
+      root 'home#index'
+      resources :devices
+    end
   end
+
 end
