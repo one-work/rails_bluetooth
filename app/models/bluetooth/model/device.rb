77443af706d99f6ee74f20e4d17596b7d3f701d5
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-03 15:58:51 +0800

    uniq

--- app/models/bluetooth/model/device.rb
+++ app/models/bluetooth/model/device.rb
@@ -6,6 +6,8 @@ module Bluetooth
       attribute :name, :string
 
       belongs_to :organ, class_name: 'Org::Organ', optional: true
+
+      validates :name, uniqueness: { scope: :organ_id }
     end
 
   end
