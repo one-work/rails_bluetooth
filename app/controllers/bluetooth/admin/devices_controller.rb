80e39aa8a165068c977d2ec0dc1f6fb6dd280265
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-03 15:53:26 +0800

    fix typo

--- app/controllers/bluetooth/admin/devices_controller.rb
+++ app/controllers/bluetooth/admin/devices_controller.rb
@@ -1,14 +1,18 @@
 module Bluetooth
   class Admin::DevicesController < Admin::BaseController
+    before_action :set_new_device, only: [:new, :create, :scan]
 
     def scan
       name, _ = params[:result].split('&')
-      @device = Device.new(device_params)
       @device.name = name
-      @deivce.save
+      @device.save
     end
 
     private
+    def set_new_device
+      @device = Device.new(device_params)
+    end
+
     def device_params
       p = params.fetch(:device, {}).permit(
         :name
